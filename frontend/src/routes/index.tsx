12e8e0b9b322be609cdb3b5fb6b343c12aff0ab0
Ketile Santos <ketile.pimenta@gmail.com>
2021-09-14 20:30:35 -0300

    Implemented page cart

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Cart } from '../pages/Cart';
 import { Events } from '../pages/Events';
 import { Home } from '../pages/Home';
 
@@ -8,6 +9,7 @@ export function Routes(): JSX.Element {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/bilhet/events" component={Events} />
+      <Route exact path="/bilhet/cart" component={Cart} />
     </Switch>
   );
 }
