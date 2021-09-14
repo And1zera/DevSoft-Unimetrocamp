3f8e7146a8f74e89387826f94f7ca504e974d188
Ketile Santos <ketile.pimenta@gmail.com>
2021-09-14 20:04:36 -0300

    Implemented quantity of items in the cart

--- frontend/src/pages/Events/index.tsx
+++ frontend/src/pages/Events/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { MdShoppingBasket } from 'react-icons/md';
 import { EventList } from '../../components/EventList';
 import { Header } from '../../components/Header';
+import { useCart } from '../../hooks/useCart';
 import { Cart } from './styles';
 
 export function Events(): JSX.Element {
+  const { cart } = useCart();
+  const cartSize = cart.length;
   return (
     <>
       <Header isColorActive>
         <Cart to="/bilhet/cart">
           <div>
             <strong>Meu Carrinho</strong>
-            <span data-testid="cart-size">0</span>
+            <span data-testid="cart-size">
+              {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+            </span>
           </div>
           <MdShoppingBasket size={36} color="#FFF" />
         </Cart>
