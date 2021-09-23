bd9df8e426132025d15f0a21e286fa4909f1a5c5
Anderson Ferreira da Silva <help1.anderson@hotmail.com>
2021-09-23 20:40:21 -0300

    Adicao da pasta de backend

---
+++ backend/Bilhet.API/Startup.cs
@@ -0,0 +1,88 @@
+using Bilhet.API.Configurations;
+using Bilhet.Repository.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Bilhet.API
+{
+    public class Startup
+    {
+        public Startup(IHostEnvironment env)
+        {
+
+            Console.WriteLine(env.EnvironmentName);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                .AddEnvironmentVariables();
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bilhet.API", Version = "v1" });
+            });
+
+            services.AddDatabaseConfiguration(Configuration);
+            services.AddDependencyInjectionConfiguration(Configuration);
+            services.AddHealthChecks().AddSqlServer(Configuration["ConnectionStrings:SQL"]);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bilhet.API v1"));
+            }
+
+            UpdateDatabase(app);
+
+            app.UseHealthChecks("/health");
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static void UpdateDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetRequiredService<IServiceScopeFactory>()
+                .CreateScope())
+            {
+                using (var context = serviceScope.ServiceProvider.GetService<BilhetContext>())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
