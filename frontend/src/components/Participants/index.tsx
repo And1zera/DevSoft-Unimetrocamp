0fbb311cfd118bf7474262842924096829a9c0a3
Ketile Santos <ketile.pimenta@gmail.com>
2021-09-29 05:36:20 -0300

    Ajuste no componente participante

--- frontend/src/components/Participants/index.tsx
+++ frontend/src/components/Participants/index.tsx
@@ -17,60 +17,45 @@ export function Participants({
   isOpen,
   id,
 }: ParticipantsProps): JSX.Element {
-  const [rgs, setRgs] = useState('');
+  const [rg, setRg] = useState('');
   const { cart, loadShoppingCart } = useCart();
-
-  const handleChangeRg = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const rg = e.target.value;
-    setRgs(rg);
-  };
-
-  const handleCloseModal = () => {
-    onCloseModal();
-  };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     cart.map(async product => {
       if (id === product.id) {
-        await api.put(`/shoppingCart/${id}`, { ...product, rgs });
+        await api.put(`/shoppingCart/${id}`, { ...product, rg });
       }
     });
     const { data } = await api.get('/shoppingCart');
     loadShoppingCart(data);
-    setRgs('');
     onCloseModal();
   };
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={handleCloseModal}
+      onRequestClose={onCloseModal}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
       <button
         type="button"
         className="react-modal-close"
-        onClick={handleCloseModal}
+        onClick={onCloseModal}
       >
         <img src={closeImg} alt="Fechar modal" />
       </button>
       <Container>
         <Content>
-          <h2>INFORMAR PARTICIPANTES</h2>
-          {cart.map(
-            product =>
-              product.id === id && (
-                <input
-                  key={product.id}
-                  type="text"
-                  placeholder="RG do bilhete"
-                  value={product.rgs}
-                  onChange={e => handleChangeRg(e)}
-                />
-              )
-          )}
+          <h2>INFORMAR PARTICIPANTE</h2>
+          <input
+            key={id}
+            type="text"
+            placeholder="RG do bilhete"
+            value={rg}
+            onChange={e => setRg(e.target.value)}
+          />
           <button type="submit" onClick={handleSubmit}>
             Salvar
           </button>
